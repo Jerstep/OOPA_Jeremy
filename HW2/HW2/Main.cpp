28268576ec021432033b086a5da10aa4464df008
Unknown <jeremy.koskamp@live.nl>
2018-12-04 01:37:14 +0100

    [/] HW 2

---
+++ HW2/HW2/Main.cpp
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+const int rows = 25, columns = 25;
+
+void begin(int rows, int columns);
+char* create(int row, int columns);
+
+int main(int argc, char* argv[]) {
+	int row
+}
+
